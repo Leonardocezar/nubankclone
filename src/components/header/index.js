a28973af3df6f7a396bfcf468966bbdfb698fcbc
Leonardocezar <leonardoc3zar@gmail.com>
2020-02-13 18:44:24 -0300

    Implemented All components and Layout

---
+++ src/components/header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container ,Top,Logo,Title} from './styles';
+import logo from '../../assets/images/Nubank_Logo.png';
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+export default function Header(){
+    return(
+        <Container>
+            <Top>
+                <Logo source={logo}/>
+                <Title>Leonardo</Title>
+            </Top>
+            <Icon name="keyboard-arrow-down" size={20} color="#fff"/>
+        </Container>
+    )
+}
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import { View
 } from 'react-native'
-import {Container} from './style'
+import Header from '../../components/header';
+import Tabs from '../../components/tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import {Container,Content,Card,CardHeader,CardContent,CardFooter,Title,Description,Annotation} from './styles'
 export default function Main(){
   return(
-    <Container/>
+    <Container>
+      <Header/>
+      <Content>
+        <Card>
+          <CardHeader>   
+            <Icon name="attach-money" size={28} color="#666"/>
+            <Icon name="visibility-off" size={28} color="#666"/>      
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo Disponivel</Title>
+            <Description>R$ 185.138,99</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferencia de R$ 2.298,11 recebida de João Carlos hoje às 17:27h
+            </Annotation>
+          </CardFooter>
+        </Card>
+
+      </Content>
+      <Tabs/>
+      </Container>
   )
 }
